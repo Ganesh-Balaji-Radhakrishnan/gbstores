3a0d1a7ecc6ec01d4285ab65287f76ea5b5891ae
Ganesh Balaji Radhakrishnan <ganesh.krishnan237@gmail.com>
2020-11-23 14:32:25 +0100

    created an ecommerece project with create react app and added homepage

---
+++ src/homepage.component.jsx
@@ -0,0 +1,45 @@
+import React from "react"
+
+
+const HomePage = () => (
+    <div className="homepage">
+        <div className="directory-menu">
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title">xyz</h1>
+                    <span className="subtitle">xyz</span>
+                </div>
+            </div>
+
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title">xyz</h1>
+                    <span className="subtitle">xyz</span>
+                </div>
+            </div>
+
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title">xyz</h1>
+                    <span className="subtitle">xyz</span>
+                </div>
+            </div>
+
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title">xyz</h1>
+                    <span className="subtitle">xyz</span>
+                </div>
+            </div>
+
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title">xyz</h1>
+                    <span className="subtitle">xyz</span>
+                </div>
+            </div>
+        </div>
+    </div>
+)
+
+export default HomePage
